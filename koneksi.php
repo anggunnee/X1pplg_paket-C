46429ecfabf07b4f76d1b82462e57623ee672d49
anggunnee <anggunnok3@gmail.com>
2024-10-24 09:13:04 +0700

    maey

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+/** 
+ * hostname = localhost
+ * username = root
+ * password =
+ * nama database = pengaduan_masyarakat
+ */
+ 
+ $koneksi =mysql_connect('localhost','root','','pengaduan_masyarakat')
+?>
