526519448cfe17fe6909e3d3ad7b41bdee40c0af
anggunnee <anggunnok3@gmail.com>
2024-11-07 14:02:18 +0700

    may

---
+++ halaman.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_GET[ 'ur1'])){
+    switch (variable) {
+        case 'value':
+            # code...
+            break;
+
+        default:
+            # code...
+            break;
+    
+    }
+}
